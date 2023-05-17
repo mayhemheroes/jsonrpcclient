6503034d6da72a15b5569644f8dc3354b56a4d78
Kayla <kayla6261@gmail.com>
2023-05-17 15:57:05 -0400

    structural fuzzer

--- mayhem/fuzz_parse.py
+++ mayhem/fuzz_parse.py
@@ -1,27 +1,55 @@
 #!/usr/bin/python3
 import atheris
 import sys
-from json import JSONDecodeError
 
 with atheris.instrument_imports():
-    from jsonrpcclient import parse_json
+    from jsonrpcclient import parse
 
 def RandomString(fdp, min_len, max_len):
     str_len = fdp.ConsumeIntInRange(min_len, max_len)
     return fdp.ConsumeUnicodeNoSurrogates(str_len)
 
+def RandomDict(fdp, key_count, allowed_depth):
+    entry_count = fdp.ConsumeIntInRange(0, key_count)
+
+    gen_dict = {}
+
+    for i in range(entry_count):
+        key = RandomString(fdp, 1, 128)
+        entry_type = fdp.ConsumeIntInRange(0, 4)
+
+        value = None
+
+        if entry_type == 0 and allowed_depth > 0:
+            value = RandomDict(fdp, key_count, allowed_depth - 1)
+        elif entry_type == 0:
+            value = fdp.ConsumeBool()
+        elif entry_type == 1:
+            value = fdp.ConsumeInt(8)
+        elif entry_type == 2:
+             value = fdp.ConsumeRegularFloat()
+        elif entry_type == 3:
+            value = RandomString(fdp, 0, 128)
+        elif entry_type == 4:
+            value = None
+           
+        gen_dict[key] = value
+
+    return gen_dict
+
 def TestOneInput(data):
     fdp = atheris.FuzzedDataProvider(data)
 
-    json_str = RandomString(fdp, 0, 4096)
+    key_count = fdp.ConsumeIntInRange(0, 128)
+    allowed_depth = fdp.ConsumeIntInRange(0, 4)
+
+    rpc_dict = RandomDict(fdp, key_count, allowed_depth)
 
     try:
-        parse_json(json_str)
+        parse(rpc_dict)
     except KeyError:
         pass
     except TypeError:
-        pass
-    except JSONDecodeError:
         pass
 
 atheris.Setup(sys.argv, TestOneInput)
